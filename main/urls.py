b4778f763e28491102504e220fa7db8030a17450
Nya2k <eudoravanya@gmail.com>
2024-04-29 22:22:18 +0700

    tayangan dan hasil pencarian"

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
+    path('tayangan_guest/', tayangan_guest, name='tayangan_guest_page'),
+    path('hasil_pencarian_guest/', hasil_pencarian_guest, name='hasil_pencarian_guest_page'),
+    path('tayangan_aktif/', tayangan_aktif, name='tayangan_aktif_page'),
+    path('hasil_pencarian_aktif/', hasil_pencarian_aktif, name='hasil_pencarian_aktif_page'),
     path('film/', hal_film_page, name='hal_film_page'),
     path('series/', hal_series_page, name='hal_series_page'),
     path('episode/', hal_episode_page, name='hal_episode_page'),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+def tayangan_guest(request):
+    return render(request, 'tayangan_guest.html')
+
+def hasil_pencarian_guest(request):
+    return render(request, 'hasil_pencarian_guest.html')
+
+def tayangan_aktif(request):
+    return render(request, 'tayangan_aktif.html')
+
+def hasil_pencarian_aktif(request):
+    return render(request, 'hasil_pencarian_aktif.html')
+
 def hal_film_page(request):
     return render(request, 'hal_film.html')
 
