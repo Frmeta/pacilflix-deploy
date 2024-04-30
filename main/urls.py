03a740f149c35db032d73201b28823026bc8f3d5
Sabina Maritza Moenzil <sabinamoenzil@icloud.com>
2024-04-30 23:08:31 +0700

    kontributor

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,9 @@ from main.views import *
 
 urlpatterns = [
     path('kontributor/', kontributor, name='kontributor_page'),
+    path('kontributor_pemain/', kontributor_pemain, name='kontributor_pemain_page'),
+    path('kontributor_sutradara/', kontributor_sutradara, name='kontributor_sutradara_page'),
+    path('kontributor_penulis_skenario/', kontributor_penulis_skenario, name='kontributor_penulis_skenario_page'),
     path('langganan/', langganan, name='langganan_page'),
     path('update_langganan/', update_langganan, name='update_langganan_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 def kontributor(request):
     return render(request, 'kontributor.html')
 
+def kontributor_pemain(request):
+    return render(request, 'kontributor_pemain.html')
+
+def kontributor_sutradara(request):
+    return render(request, 'kontributor_sutradara.html')
+
+def kontributor_penulis_skenario(request):
+    return render(request, 'kontributor_penulis_skenario.html')
+
 def langganan(request):
     return render(request, 'langganan.html')
 
