98ae3b846343e5e184d4d79330862e8e889a86d9
Nya2k <eudoravanya@gmail.com>
2024-05-02 21:52:05 +0700

    yeay fe doneee

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('film/', hal_film_page, name='hal_film_page'),
     path('series/', hal_series_page, name='hal_series_page'),
     path('episode/', hal_episode_page, name='hal_episode_page'),
-    path('ulasan/', ulasan_page, name='ulasan_page'),
 
     # fredo
     path('daftar_unduhan/', daftar_unduhan, name='daftar_unduhan'),
